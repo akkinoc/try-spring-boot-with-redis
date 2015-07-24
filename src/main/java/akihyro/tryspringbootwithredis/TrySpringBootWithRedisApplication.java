bd29dc4142aa5dbc493174f6fce021ae91c63631
Akihiro Kondo <akihyro@users.noreply.github.com>
2015-07-24 12:59:29 +0900

    Spring Boot アプリケーションを作成

---
+++ src/main/java/akihyro/tryspringbootwithredis/TrySpringBootWithRedisApplication.java
@@ -0,0 +1,13 @@
+package akihyro.tryspringbootwithredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrySpringBootWithRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrySpringBootWithRedisApplication.class, args);
+    }
+
+}
